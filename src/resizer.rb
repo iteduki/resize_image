5ec68a16400f5a33ce674d547d6d0a7f549ec670
Takahiro Ogawa <t_ogawa@timedia.co.jp>
2019-07-03 11:47:40 +0900

    width,heightを引数にした

--- src/resizer.rb
+++ src/resizer.rb
@@ -3,16 +3,17 @@
 require 'RMagick'
 
 class Resizer
-  def self.resize
-    files = Dir.glob('./images/*')
+  def self.resize(width: 300, height: 300)
+    files = Dir.glob('./images/*').sort
 
     p "ファイル数: #{files.count}"
 
     files.each do |fileName|
+      p fileName
       img = Magick::ImageList.new(fileName)
       # 300×300
-      new_img = img.resize_to_fit(300, 300)
-      new_img.write('./resized/new_' + fileName.split('/')[1])
+      new_img = img.resize_to_fit(width, height)
+      new_img.write("./resized/#{fileName.split('/').last}")
     end
   end
 end
